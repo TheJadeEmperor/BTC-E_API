d2f568e65dd3dbb479269ee31e49853906c875f0
TheJadeEmperor <louie.social.media@gmail.com>
2017-01-24 17:22:27 -0500

    0

--- index.php
+++ index.php
@@ -3,10 +3,41 @@ include('include/api_database.php');
 include('include/api_poloniex.php');
 include('include/config.php');
 
-$candleData = new Database($db);
-
-
+//$candleData = new Database($db);
 //$candleData->sendMail();
+
+$polo = new poloniex();
+
+$BTC_ETH = $polo->get_ticker('BTC_ETH');
+
+$USDT_BTC = $polo->get_ticker('USDT_BTC');
+
+$USDT_ETH = $polo->get_ticker('USDT_ETH');
+
+
+
+?>
+
+<pre class="xdebug-var-dump">
+<table>
+<tr>
+	<td>ETH/BTC </td>
+	<td><?=$BTC_ETH['last']; ?>
+</td>
+</tr>
+	<td>BTC/USDT </td>
+	<td> <?=$USDT_BTC['last'] ?> </td>
+</tr>
+</tr>
+	<td>ETH/USDT </td>
+	<td> <?=$USDT_ETH['last']?> </td>
+</tr>
+</table>
+</pre>
+
+
+<?
+exit;
 
 include('index1.html');
 ?>
