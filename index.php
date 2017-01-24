5d9bf5ab9c73aa27928434b76a38ac9522b210be
TheJadeEmperor <louie.social.media@gmail.com>
2017-01-24 16:37:23 -0500

    index

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 include('include/api_database.php');
-include('include/api_bitfinex.php');
+include('include/api_poloniex.php');
 include('include/config.php');
 
 $candleData = new Database($db);
 
 
 //$candleData->sendMail();
+
+include('index1.html');
 ?>
