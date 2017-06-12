451d390d3c6f73ba3337eecfb0b31065cdc478fe
TheJadeEmperor <louie.online.acc@gmail.com>
2017-06-12 00:34:01 -0400

    cronAutoTrade

---
+++ cronAutoTrade.php
@@ -0,0 +1,85 @@
+<?php
+$dir = 'include/';
+include($dir.'api_database.php');
+include($dir.'api_poloniex.php');
+include($dir.'config.php');
+include($dir.'ez_sql_core.php');
+include($dir.'ez_sql_mysql.php');
+
+
+//set timezone
+date_default_timezone_set('America/New_York');
+
+//get timestamp
+$currentTime = date('Y-m-d H:i:s', time());
+
+//database connection
+$db = new ezSQL_mysql($dbUser, $dbPW, $dbName, $dbHost);
+
+
+$debug = $_GET['debug'];
+
+//connect to Poloniex
+$polo = new poloniex($polo_api_key, $polo_api_secret);
+
+
+$tickerArray = $polo->get_ticker(); //ticker prices
+$balanceArray = $polo->get_balances(); //account balances
+
+//print_r($balanceArray);
+
+foreach($tickerArray as $currencyPair => $tickerData) {
+	
+	$percentChange = $tickerData['percentChange'];
+	$lastPrice = $tickerData['last']; //most recent price for this coin
+	
+	list($market, $curr) = explode('_',  $currencyPair);
+	
+	$percentChangeFormat = $percentChange * 100;
+	
+	$percentChangeFormat = number_format($percentChangeFormat, 2);
+	
+	if($market == 'BTC') //only show BTC markets
+	if($percentChangeFormat > 15 && $percentChangeFormat < 20) { //check if price > 15% && price < 20%
+
+		
+		
+		
+		//check if there's a balance for the currencyPair
+		
+		if($balanceArray[$curr] == 0) { //if no balance, then buy
+			$balanceDisplay = ' No balance ';
+			
+			//echo $lastPrice .' ';
+			$tradeAmount = 0.1 / $lastPrice;
+
+			/*
+until = 2 weeks
+*/
+			
+	echo $dateInTwoWeeks = strtotime('+2 weeks');		
+			
+			//buy order
+			if($debug != 1) {
+				$tradeResult = $polo->buy($currencyPair, $lastPrice, $tradeAmount); 
+			}
+			
+			//set stop loss through btc_trades table - sell if < 12%
+			$insert = "INSERT INTO $tradeTable (trade_exchange, trade_currency, trade_condition, trade_price, trade_action, trade_amount, trade_unit, until) values ('Poloniex', '".$currencyPair."', '<', '12', 'Sell', '".$tradeAmount."', '%', '".$until."' )";
+			
+			//create new record in trade table for currencyPair
+
+			
+		}
+		else { //there is a balance
+			$balanceDisplay = $balanceArray[$curr];
+		}
+		
+		echo $output = '<br />'.$currencyPair.' +'.$percentChangeFormat.'% | '.$balanceDisplay .' | lastPrice: '.$lastPrice.' | tradeAmount: '.$tradeAmount;
+	}
+	
+}
+
+
+print_r($tradeResult);
+?>
