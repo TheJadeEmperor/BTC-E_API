b8b13e68fe07f362e56efed16048640e98461bb5
TheJadeEmperor <louie.social.media@gmail.com>
2017-06-15 15:41:27 -0400

    update stopLoss & fee adjustment for tradeAmount

--- cronAutoTrade.php
+++ cronAutoTrade.php
@@ -9,6 +9,8 @@ include($dir.'ez_sql_mysql.php');
 //set timezone
 date_default_timezone_set('America/New_York');
 
+$debug = $_GET['debug'];
+
 global $db;
 global $currentTime;
 
@@ -16,17 +18,20 @@ global $currentTime;
 $currentTime = date('Y-m-d H:i:s', time());
 
 //database connection
-
 $db = new ezSQL_mysql($dbUser, $dbPW, $dbName, $dbHost);
-
-$debug = $_GET['debug'];
 
 //connect to Poloniex
 $polo = new poloniex($polo_api_key, $polo_api_secret);
 
-
 $tickerArray = $polo->get_ticker(); //ticker prices
 $balanceArray = $polo->get_balances(); //account balances
+
+if($debug == 1) {
+	$newline = '<br />';
+}
+else {
+	$newline = "\n";
+}
 
 //print_r($balanceArray);
 
@@ -41,31 +46,37 @@ foreach($tickerArray as $currencyPair => $tickerData) {
 	$percentChangeFormat = $percentChange * 100;
 	
 	$percentChangeFormat = number_format($percentChangeFormat, 2);
+
+	$stopLoss = $percentChangeFormat - 2;
 	
 	if($market == 'BTC') //only show BTC markets
 	if($percentChangeFormat > 15 && $percentChangeFormat < 20) { //check if price > 15% && price < 20%
 
-		
+		$tradeAmount = 0.1 / $lastPrice;
+
+		//minus trading fees
+		$tradeAmountAfterFees = $tradeAmount - $tradeAmount * 0.0015;
+			
+
 		//check if there's a balance for the currencyPair
 		if($balanceArray[$curr] == 0) { //if no balance, then buy
 			$balanceDisplay = ' No balance ';
 			
-			//echo $lastPrice .' ';
-			$tradeAmount = 0.1 / $lastPrice;
-
+			
+		
 			$dateInTwoWeeks = strtotime('+2 weeks');		
 			$until = date('Y-m-d h:i:m', $dateInTwoWeeks);
 					
 			//buy order
 			if($debug != 1) {
 				$tradeResult = $polo->buy($currencyPair, $lastPrice, $tradeAmount); 
-			}
 			
-			//set stop loss through btc_trades table - sell if < 12%
-			$insert = "INSERT INTO $tradeTable (trade_exchange, trade_currency, trade_condition, trade_price, trade_action, trade_amount, trade_unit, until) values ('Poloniex', '".$dbCurrencyPair."', '<', '12', 'Sell', '".$tradeAmount."', '%', '".$until."' )";
 			
-			//create new record in trade table for currencyPair
-			if($debug != 1) {
+				//set stop loss through btc_trades table 
+				$insert = "INSERT INTO $tradeTable (trade_exchange, trade_currency, trade_condition, trade_price, trade_action, trade_amount, trade_unit, until) values ('Poloniex', '".$dbCurrencyPair."', '<', '".$stopLoss."', 'Sell', '".$tradeAmountAfterFees."', '%', '".$until."' )";
+				
+				//create new record in trade table for currencyPair
+		
 				 $success = $db->query($insert);
 				if($success == 1) 
 					echo '<br />Added record '.$insert.'<br />';
@@ -78,7 +89,7 @@ foreach($tickerArray as $currencyPair => $tickerData) {
 			$balanceDisplay = $balanceArray[$curr];
 		}
 		
-		echo $output = '<br />'.$currencyPair.' +'.$percentChangeFormat.'% | '.$balanceDisplay .' | lastPrice: '.$lastPrice.' | tradeAmount: '.$tradeAmount;
+		echo $output = $currencyPair.' +'.$percentChangeFormat.'% | '.$balanceDisplay .' | lastPrice: '.$lastPrice.' | stopLoss: '.$stopLoss.'% | tradeAmount: '.$tradeAmount.' | tradeAmountAfterFees: '.$tradeAmountAfterFees.$newline.$newline;
 	}
 	
 }
