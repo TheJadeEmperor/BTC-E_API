2676b9cdfe3b49fdc9fc687051d80ba84e6fd2e5
TheJadeEmperor <benjamin_louie@drugfree.org>
2018-10-02 15:33:23 -0400

    margin script update

--- test_margin.php
+++ test_margin.php
@@ -4,34 +4,80 @@ include($dir.'api_database.php');
 include($dir.'api_poloniex.php');
 include($dir.'config.php');
 include($dir.'ez_sql_core.php');
-include($dir.'ez_sql_mysql.php');
-
-
+include($dir.'ez_sql_mysql.php'); 
 
 //requires the extension php_openssl to work
 $polo = new poloniex($polo_api_key, $polo_api_secret);
 
-echo $currencyPair = 'BTC_ETH';
-$amt = '0.1';
-$rate = '0.0348';
+//database connection
+$db = new ezSQL_mysql($dbUser, $dbPW, $dbName, $dbHost);
+
+//connect to the BTC Database
+$newDB = new Database($db);
+
+$debug = 1;
 
 
-//$rrr = $polo->buy($currencyPair, '0.0569', $amt, 1);
+
+echo $currencyPair = 'BTC_XRP';
+$amt = '250';
+
+
+
+//$rrr = $polo->close_margin_position($currencyPair, $order_number);
+//print_r($rrr);
 
 
 //$rrr = $polo->margin_sell($currencyPair, $rate, $amt, 1);
 //print_r($rrr);
 
-//gmail API 
 
+//gmail API here
 //if criteria is met
+//From: noreply@tradingview.com 
+//Subject: TradingView Alert: Short Signal 
 
-//check for open pos & close it
+if($criteria_is_met) {
+			
+	$currencyPair = 'BTC_ETH'; 
+	$amount = '0.01';
 
-//open margin pos - long or short
+	//get current price of pair
 
-//amount is 0.01 
-//
+	
+	$rate = 'current rate';  
+	$output .= $currencyPair.' '.$amount.' '.$rate.' ';		
+	
+	
+	//check for open pos & close it
+	if($debug == 0) {
+		$closePos = $polo->close_margin_position($currencyPair, $order_number);
+			
+		print_r($closePos); 
+		
+	}
+		
+
+	//after upgrading tradingview
+	//open margin pos - long 
+	//$shortPos = $polo->margin_buy($currencyPair, $rate, $amt, 1);
+
+	if($debug == 0) {
+		//open margin pos - short 
+		$shortPos = $polo->margin_sell($currencyPair, $rate, $amt, 1);
+
+		print_r($shortPos);
+	}
+
+	//send text message
+	$sendMailBody = 'Opened Short Pos on '.$currencyPair.' | Amount: '.$amount.'';
+	$output .= $sendMailBody;		
+	if($debug == 0) {
+		$newDB->sendMail($sendEmailBody); 
+	}
+
+	echo $output;
+}
 
 
 ?>
