eda0d675b8d9e98dde5217bcf55cce14044b1cad
TheJadeEmperor <benjamin_louie@drugfree.org>
2018-10-16 14:57:08 -0400

    script update

--- script_tv_polo1.php
+++ script_tv_polo1.php
@@ -15,11 +15,12 @@ $newDB = new Database($db);
 $newDB->database($dbHost, $dbUser, $dbPW, $dbName);
 
 
-
 $debug = 0; 
 
-$currencyPair = 'BTC_XRP'; 
-$amount = '100';
+$currencyPair = 'USDT_BTC'; 
+$amount = '0.01';
+$orderType = 'openOrder';
+
 //get current price of pair
 $currentRate = $polo->get_ticker($currencyPair);
 
@@ -33,8 +34,8 @@ $mails = new EmailImporter( '{host187.hostmonster.com:993/imap/ssl}INBOX', $gmai
 
 
 //search for these subject lines
-$subjectSignalLong = "TradingView Alert: Long Signal";
-$subjectSignalShort = "TradingView Alert: Short Signal";
+$subjectSignalLong = "TradingView Alert: XBTUSD Long Signal";
+$subjectSignalShort = "TradingView Alert: XBTUSD Short Signal";
 
 $matchedMailsLong = $mails->getMailsBySubject($subjectSignalLong);
 $matchedMailsShort = $mails->getMailsBySubject($subjectSignalShort);
@@ -53,64 +54,49 @@ if($matchedMailsShort[0]['subject'] == $subjectSignalShort) {
 }
 
 	
-//check for open margin order & replace it
+//check for open order & replace it using move_order()
 //to keep orders fresh and make sure orders go through
 if($debug == 0) {
 	$openOrders = $polo->return_open_orders($currencyPair);
 		 
 	echo 'openOrders '; 
-	var_dump($openOrders);
-	if($openOrders[0]['margin'] == 1) 
-		$orderNumber = $openOrders[0]['orderNumber'];
 	
-	if(isset($orderNumber)) {
-		$moveOrder = $polo->move_order($orderNumber, $rate);
-		echo 'moveOrder ';
-		var_dump($moveOrder);
+	var_dump($openOrders);
+	
+	foreach($openOrders as $num => $order) {
+		$orderNumber = $order['orderNumber'];
+		$orderType = $order['orderType'];
+		
+		if($orderType == 'sell' || $orderType == 'buy') {
+			$moveOrder = $polo->move_order($orderNumber, $rate);
+			echo 'moveOrder ';
+			var_dump($moveOrder);
+		}
 	}
+	 
 }
-
-
-//check for open pos - poloniex
-$getMarginPos = $polo->get_margin_position($currencyPair);
-echo 'getMarginPos: ';
-
-if($getMarginPos['type'] == 'none') { //no positions open
-	echo ' none <br />';
-}
-else {
-	var_dump($getMarginPos); 
-	$orderNumber = $getMarginPos['orderNumber'];
-}
-
 
 
 if($criteria_is_met) {
 
-	//close pos befor opening new pos 
-	$closeMarginPos = $polo->close_margin_position($currencyPair, $marginOrderNumber);
-	echo 'closeMarginPos: ';
-	var_dump($closeMarginPos); 
-	
-
-	if($short_signal) { //open margin pos - short
+	if($short_signal) { //open pos - short
 		if($debug == 0) {
-			$shortPos = $polo->margin_sell($currencyPair, $rate, $amount, 1);
-			echo $typePos = 'shortPos'; 
+			$shortPos = $polo->sell($currencyPair, $rate, $amount, $orderType);
+			echo $typePos = 'Sell '; 
 			var_dump($shortPos); 
 		}
 	}
 	
-	if ($long_signal) { //open margin pos - long
+	if ($long_signal) { //open pos - long
 		if($debug == 0) {
-			$longPos = $polo->margin_buy($currencyPair, $rate, $amount, 1);
-			echo $typePos = 'longPos';
+			$longPos = $polo->buy($currencyPair, $rate, $amount, $orderType);
+			echo $typePos = 'Buy ';
 			var_dump($longPos);
 		}
 	}
 		
 	//send text message
-	$sendMailBody = 'Opened '.$typePos.' on Poloniex for '.$currencyPair.' | Amount: '.$amount.'';
+	$sendMailBody = 'Action: '.$typePos.' '.$currencyPair.' on Poloniex for Amount: '.$amount.' ';
 
 	
 	if($debug == 0) {
