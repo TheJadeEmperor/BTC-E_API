b9041caaf1236ea574fef632cc439d9fae774d17
TheJadeEmperor <benjamin_louie@drugfree.org>
2018-10-16 23:49:23 -0400

    Update script

--- script_tv_polo.php
+++ script_tv_polo.php
@@ -3,8 +3,6 @@ $dir = 'include/';
 include($dir.'api_database.php');
 include($dir.'api_poloniex.php');
 include($dir.'config.php');
-//include($dir.'ez_sql_core.php');
-//include($dir.'ez_sql_mysql.php'); 
 include($dir.'api_imap_class.php');
 
 
@@ -16,105 +14,110 @@ $newDB = new Database($db);
 
 $newDB->database($dbHost, $dbUser, $dbPW, $dbName);
 
-//$newDB->showMail();
 
 
 $debug = 0; 
 
 $currencyPair = 'BTC_XRP'; 
-$amount = '10';
+$amount = '100';
+//get current price of pair
+$currentRate = $polo->get_ticker($currencyPair);
 
-echo 'currencyPair: '. $currencyPair.' | amount: '.$amount.''; 
+$rate = $currentRate['last'];
+
+echo 'currencyPair: '. $currencyPair.' | amount: '.$amount.' | rate: '.$rate.' <br />'; 
+
 
 //connect to imap service
 $mails = new EmailImporter( '{host187.hostmonster.com:993/imap/ssl}INBOX', $gmail_username, $gmail_password);
 
- 
 
+//search for these subject lines
 $subjectSignalLong = "TradingView Alert: Long Signal";
 $subjectSignalShort = "TradingView Alert: Short Signal";
 
 $matchedMailsLong = $mails->getMailsBySubject($subjectSignalLong);
 $matchedMailsShort = $mails->getMailsBySubject($subjectSignalShort);
 
-print_r($matchedMailsLong[0]['subject']);
-print_r($matchedMailsShort[0]['subject']);
-
-
+//subject line is found - long signal
 if($matchedMailsLong[0]['subject'] == $subjectSignalLong) { 
-	echo 'long '; 
+	print_r($matchedMailsLong[0]['subject']);
 	$criteria_is_met = true;
 	$long_signal = true;
 }
-
+//subject line is found - short signal
 if($matchedMailsShort[0]['subject'] == $subjectSignalShort) { 
-	echo 'short'; 
+	print_r($matchedMailsShort[0]['subject']);
 	$criteria_is_met = true;
 	$short_signal = true;
 }
 
+	
+//check for open margin order & replace it
+//to keep orders fresh and make sure orders go through
+if($debug == 0) {
+	$openOrders = $polo->return_open_orders($currencyPair);
+		 
+	echo 'openOrders '; 
+	var_dump($openOrders);
+	if($openOrders[0]['margin'] == 1) 
+		$orderNumber = $openOrders[0]['orderNumber'];
+	
+	if(isset($orderNumber)) {
+		$moveOrder = $polo->move_order($orderNumber, $rate);
+		echo 'moveOrder ';
+		var_dump($moveOrder);
+	}
+}
+
+
+//check for open pos - poloniex
+$getMarginPos = $polo->get_margin_position($currencyPair);
+echo 'getMarginPos: ';
+
+if($getMarginPos['type'] == 'none') { //no positions open
+	echo ' none <br />';
+}
+else {
+	var_dump($getMarginPos); 
+	$orderNumber = $getMarginPos['orderNumber'];
+}
+
+
 
 if($criteria_is_met) {
 
-	//get current price of pair
-	$currentRate = $polo->get_ticker($currencyPair);
+	//close pos befor opening new pos 
+	$closeMarginPos = $polo->close_margin_position($currencyPair, $marginOrderNumber);
+	echo 'closeMarginPos: ';
+	var_dump($closeMarginPos); 
+	
 
-	$rate = $currentRate['last'];
-	echo ' | rate: '.$rate.' '; 
-
-	//check for open pos - poloniex
-	$getMarginPos = $polo->get_margin_position($currencyPair);
-	echo 'getMarginPos ';
-	var_dump($getMarginPos); 
-	
-	if($getMarginPos['type'] == 'none') //no positions open
-		$adjustedAmount = $amount;
-	else {
-		$adjustedAmount = $amount * 2;
-	}
-	
-	
-	//check for open margin order & replace it
-	if($debug == 0) {
-		$openOrders = $polo->return_open_orders($currencyPair);
-			 
-		echo 'openOrders '; 
-		var_dump($openOrders);
-		if($openOrders[0]['margin'] == 1) 
-			$orderNumber = $openOrders[0]['orderNumber'];
-		
-		if(isset($orderNumber)) {
-			$moveOrder = $polo->move_order($orderNumber, $rate);
-			echo 'moveOrder ';
-			var_dump($moveOrder);
-		}
-	}
-	
 	if($short_signal) { //open margin pos - short
 		if($debug == 0) {
-			//$shortPos = $polo->margin_sell($currencyPair, $rate, $adjustedAmount, 1);
+			$shortPos = $polo->margin_sell($currencyPair, $rate, $amount, 1);
 			echo $typePos = 'shortPos'; 
 			var_dump($shortPos); 
 		}
 	}
-	else if ($long_signal) { //open margin pos - long
+	
+	if ($long_signal) { //open margin pos - long
 		if($debug == 0) {
-			//$longPos = $polo->margin_buy($currencyPair, $rate, $adjustedAmount, 1);
+			$longPos = $polo->margin_buy($currencyPair, $rate, $amount, 1);
 			echo $typePos = 'longPos';
 			var_dump($longPos);
 		}
 	}
-	
-	
+		
 	//send text message
 	$sendMailBody = 'Opened '.$typePos.' on Poloniex for '.$currencyPair.' | Amount: '.$amount.'';
-	$output .= $sendMailBody;		
+
 	
 	if($debug == 0) {
 		$newDB->sendMail($sendMailBody); 
 	} 
 
-	echo '<br />'.$output;
+	echo '<br />'.$sendMailBody;
 }
 
 
