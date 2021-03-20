4d34783404db28edc2331a1d499fe0ba9d304d65
TheJadeEmperor <benjamin_louie@drugfree.org>
2021-03-19 23:07:33 -0400

    new btc dashboard

--- dashboard.php
+++ dashboard.php
@@ -9,7 +9,40 @@ if(!isset($_SESSION['admin']))//if not logged in, redirect back to login page
 
 //set timezone
 date_default_timezone_set('America/New_York');
+ 
+//alert mysql ajax calls
+$createAlert = 'include/ajax.php?action=create';
+$readAlert = 'include/ajax.php?action=read';
+$updateAlert = 'include/ajax.php?action=update';
+$deleteAlert = 'include/ajax.php?action=delete';
 
+//trade mysql jax calls
+$createTrade = 'include/ajax.php?action=createTrade';
+$readTrade = 'include/ajax.php?action=readTrade';
+$updateTrade = 'include/ajax.php?action=updateTrade';
+$deleteTrade = 'include/ajax.php?action=deleteTrade';
+
+//page load ajax calls
+$loadCronSendAlerts = 'load.php?page=cronSendAlerts&accessKey='.$accessKey;
+$loadbtcTrades = 'load.php?page=btcTrades&accessKey='.$accessKey;
+$loadPriceTable = 'load.php?page=priceTable&accessKey='.$accessKey;
+$loadCronAutoTrade = 'load.php?page=cronAutoTrade&accessKey='.$accessKey;
+$loadBalanceTable = 'load.php?page=balanceTable&accessKey='.$accessKey;
+
+$loadBitmexPositions = 'load.php?page=bitmexPositions&accessKey='.$accessKey;
+$loadBitmexPositions2 = 'load.php?page=bitmexPositions2&accessKey='.$accessKey;
+
+$loadNotesAjax = 'include/ajax.php?action=updateNotes';
+$loadNotes = 'load.php?page=notes&accessKey='.$accessKey;
+
+$loadKCMBalance = 'load/kucoinBalance.php?page=kucoinMainBalance&accessKey='.$accessKey;
+$loadKC1Balance = 'load.php?page=kucoin1Balance&accessKey='.$accessKey;
+$loadKC2Balance = 'load.php?page=kucoin2Balance&accessKey='.$accessKey;
+$loadKC3Balance = 'load.php?page=kucoin3Balance&accessKey='.$accessKey;
+$loadKC4Balance = 'load.php?page=kucoin4balance&accessKey='.$accessKey;
+$loadKC5Balance = 'load.php?page=kucoin5Balance&accessKey='.$accessKey;
+
+$loadBtrexBalance = 'load/btrexBalance.php?accessKey='.$accessKey;
 
 ?>
 <head>
@@ -27,6 +60,7 @@ date_default_timezone_set('America/New_York');
 
 	<script src="include/jquery-ui/ui/jquery-ui.js"></script>
 	
+    <link rel="stylesheet" href="include/admin.css" />
 	<link rel="shortcut icon" type="image/png" href="include/dollar_sign.png">
 
     <?
@@ -38,14 +72,26 @@ date_default_timezone_set('America/New_York');
 
 <div class="row">
     <div class="col-6">
-        <div id="btrexBalance"></div>
+        <div id="btrexBalance">btrexBalance</div>
         
-        <div id="kucoinBalance"></div>
+        <div id="kucoinMainBalance">kucoinMainBalance</div>
 
-        <div id="kucoinSubaccounts"></div>
+        <div id="kucoin1Balance"></div>
 
-        <div id="binanceBalance"></div>
+        <div id="kucoin2Balance"></div>
+
+        <div id="kucoin3Balance"></div>
+
+        <div id="kucoin4Balance"></div>
+
+        <div id="kucoin5Balance"></div>
         
+        <br />
+    </div>
+
+    <div class="col">
+		
+      
         <br />
     </div>
 </div>
