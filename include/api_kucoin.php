c09c0ef87d3f0df8e722edf69d69548c4f782958
TheJadeEmperor <benjamin_louie@drugfree.org>
2021-03-21 17:48:25 -0400

    get kucoin balances | api_kucoin - make class

--- include/api_kucoin.php
+++ include/api_kucoin.php
@@ -1,6 +1,261 @@
 <?php
+class Kucoin {
+  private $host = 'https://api.kucoin.com'; //production
+  //$host = 'https://openapi-sandbox.kucoin.com'; //sandbox
+  private $apiVersion;
+  private $key;
+  private $secret; 
+  private $passphrase; 
+
+  public function __construct ($key, $secret, $passphrase) {
+    $this->apiVersion = 'v1.1';
+    $this->key = $key;
+    $this->secret = $secret; 
+    $this->passphrase = $passphrase;
+    
+	}
+
+
+  public function signature($request_path = '', $body = '', $timestamp = false, $method = 'GET') {
+    // global $secret;
+    $secret = $this->secret;
+  
+    $body = is_array($body) ? json_encode($body) : $body; // Body must be in json format
+    $timestamp = $timestamp ? $timestamp : time() * 1000;
+    
+    $what = $timestamp . $method . $request_path . $body;
+    
+    return base64_encode(hash_hmac("sha256", $what, $secret, true));
+  }
+
+  public function checkBalance() {
+    // global $host;
+    // global $key;
+    // global $passphrase;
+    $host = $this->host;
+    $key = $this->key;
+    $passphrase = $this->passphrase;
+
+    $method = 'GET';
+    $request_path = '/api/v1/accounts';
+    $timestamp = time() * 1000;
+    $body = '';
+  
+    $curl = curl_init();
+  
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $host . $request_path,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => $method,
+      CURLOPT_HTTPHEADER => array(
+        "Content-Type:application/json",
+        "KC-API-SIGN:".$this->signature($request_path, $body, $timestamp, $method),
+        "KC-API-TIMESTAMP:".$timestamp,
+        "KC-API-KEY:".$key,
+        "KC-API-PASSPHRASE:".$passphrase
+      ),
+    ));
+  
+    $response = curl_exec($curl);
+    $responseArr = json_decode($response,true);
+  
+    curl_close($curl);
+  
+    $responseArr['function'] = 'checkBalance()';
+    //echo '<pre>'; print_r($responseArr); echo '</pre>';
+    return $responseArr;
+  }
+
+  public function getMarketPrice($currencyPair) {
+    global $host;
+    global $key;
+    global $passphrase;
+  
+    $method = 'GET';
+    $request_path = '/api/v1/market/orderbook/level1?symbol='.$currencyPair;
+    $timestamp = time() * 1000;
+    $body = '';
+  
+    $curl = curl_init();
+  
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $host . $request_path,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => $method,
+      CURLOPT_HTTPHEADER => array(
+        "Content-Type:application/json",
+        "KC-API-SIGN:".signature($request_path, $body, $timestamp, $method),
+        "KC-API-TIMESTAMP:".$timestamp,
+        "KC-API-KEY:".$key,
+        "KC-API-PASSPHRASE:".$passphrase
+      ),
+    ));
+  
+    $response = curl_exec($curl);
+    $responseArr = json_decode($response,true);
+  
+    curl_close($curl);
+  
+    $responseArr['function'] = 'getMarketPrice($currencyPair)';
+    $responseArr['currencyPair'] = $currencyPair;
+    //echo '<pre>'; print_r($responseArr); echo '</pre>';
+    return $responseArr;
+  }
+  
+
+  //type is market or limit
+  function buyOrder($type, $pair, $buyQT, $ask) {
+    global $host;
+    global $key;
+    global $passphrase;
+  
+    $method = 'POST';
+    $request_path = '/api/v1/orders';
+    $timestamp = time() * 1000;
+    $body ='{"side":"buy","symbol":"'.$pair.'","type":"'.$type.'","price":"'.$ask.'","size":"'.$buyQT.'","clientOid":"'.microtime(true).'"}';
+  
+    $curl = curl_init();
+  
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $host . $request_path,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => $method,
+      CURLOPT_POST => 1,
+      CURLOPT_POSTFIELDS => $body,
+      CURLOPT_HTTPHEADER => array(
+        "Content-Type:application/json",
+        "KC-API-SIGN:".signature($request_path, $body, $timestamp, $method),
+        "KC-API-TIMESTAMP:".$timestamp,
+        "KC-API-KEY:".$key,
+        "KC-API-PASSPHRASE:".$passphrase
+      ),
+    ));
+  
+    $response = curl_exec($curl);
+    $responseArr = json_decode($response,true);
+  
+    curl_close($curl);
+  
+    $responseArr['function'] = 'function buyOrder($type, $pair, $buyQT, $ask)';
+    echo '<pre>'; print_r($responseArr); echo '</pre>';
+    return $responseArr;
+  }
+  
+  function sellOrder($type, $pair, $sellQT, $ask) {
+    global $host;
+    global $key;
+    global $passphrase;
+  
+    $method = 'POST';
+    $request_path = '/api/v1/orders';
+    $timestamp = time() * 1000;
+    $body ='{"side":"sell","symbol":"'.$pair.'","type":"'.$type.'","price":"'.$ask.'","size":"'.$sellQT.'","clientOid":"'.microtime(true).'"}';
+  
+    $curl = curl_init();
+  
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $host . $request_path,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => $method,
+      CURLOPT_POST => 1,
+      CURLOPT_POSTFIELDS => $body,
+      CURLOPT_HTTPHEADER => array(
+        "Content-Type:application/json",
+        "KC-API-SIGN:".signature($request_path, $body, $timestamp, $method),
+        "KC-API-TIMESTAMP:".$timestamp,
+        "KC-API-KEY:".$key,
+        "KC-API-PASSPHRASE:".$passphrase
+      ),
+    ));
+  
+    $response = curl_exec($curl);
+    $responseArr = json_decode($response,true);
+  
+    curl_close($curl);
+  
+    $responseArr['function'] = 'function sellOrder($type, $pair, $sellQT, $ask)';
+    echo '<pre>'; print_r($responseArr); echo '</pre>';
+    return $responseArr;
+  }
+  
+  /**
+  Array (
+      [code] => 200000
+      [data] => Array
+          (
+              [cancelledOrderIds] => Array
+                  (
+                      [0] => 602370228313f700068bc252
+                  )
+  
+          )
+  
+  )
+  */
+  function cancelOrder($orderId) {
+    global $host;
+    global $key;
+    global $passphrase;
+  
+    $method = 'DELETE';
+    $request_path = '/api/v1/orders/'.$orderId;
+    $timestamp = time() * 1000;
+    $body = '';
+  
+    $curl = curl_init();
+  
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $host . $request_path,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => $method,
+      CURLOPT_HTTPHEADER => array(
+        "Content-Type:application/json",
+        "KC-API-SIGN:".signature($request_path, $body, $timestamp, $method),
+        "KC-API-TIMESTAMP:".$timestamp,
+        "KC-API-KEY:".$key,
+        "KC-API-PASSPHRASE:".$passphrase
+      ),
+    ));
+  
+    $response = curl_exec($curl);
+    $responseArr = json_decode($response,true);
+  
+    curl_close($curl);
+  
+    $responseArr['function'] = 'cancelOrder($orderId)';
+    echo '<pre>'; print_r($responseArr); echo '</pre>';
+    return $responseArr;
+  }
+   
+}
+
 $host = 'https://api.kucoin.com'; //production
-//$host = 'https://openapi-sandbox.kucoin.com'; //sandbox
+
 
 function signature($request_path = '', $body = '', $timestamp = false, $method = 'GET') {
   global $secret;
--- load/kucoinBalance.php
+++ load/kucoinBalance.php
@@ -11,119 +11,132 @@ if($_GET['accessKey'] != 'AllLivesMatter') {
 	echo "Wrong access key"; exit;
 }
 
-function displayKCBalance($getBalances) {
-	$totalBalance = 0;
 
-	foreach($getBalances['data'] as $index) { //go through each coin you have
-        $available = $index['available'];
-    
-        if($index['currency'] == $coin[1] && $available > 0) { //match coin symbol   
-            $coinBalance = $available; 
-            if($index['available'] > 0) { //check for available balance
-                $sellQT = $index['available']; 
-                $totalBalance += $sellQT * $bid;
-            }
-        }
 
-        if($index['currency'] == 'USDT' && $available > 0) {
-            $USDTBalance = $available; 
-            $totalBalance += $USDTBalance; //add to totalBalance
-            $buyQT = $USDTBalance/$ask; //quantity to buy
-            $buyQT = $buyQT * $percentBalance; 
-        }
-	}
+function displayKCTable($subAccount, $apiFields) {
+	
+	$output = '
+	<table class="table">
+	<thead class="thead-default">
+	<tr>
+		<th colspan="8">'.$subAccount.'	<img src="include/refresh.png" class="clickable" onclick="javascript:'.$subAccount.'()" width="25px" /> </th>
+	</tr>
+	<tr>
+		<th>Currency</th><th>Balance</th><th>Price</th><th>BTC Value</th><th>USDT</th>
+	</tr>
+';
 
-	//orders only take 4 decimals
-	$buyQT = number_format($buyQT, 4, '.', '');
-	$sellQT = number_format($sellQT, 4, '.', '');
-	$coinBalance = number_format($coinBalance, 4, '.', '');
-	$USDTBalance = number_format($USDTBalance, 4, '.', '');
-	$totalBalance = number_format($totalBalance, 4, '.', '');
 
-	echo $USDTBalance.' '.$totalBalance;
+	$apiKey = $apiFields['apiKey'];
+	$apiSecret = $apiFields['apiSecret'];
+	$passphrase = $apiFields['passphrase'];
 
+	$Kucoin = new Kucoin($apiKey, $apiSecret, $passphrase);
+	$getBalances = $Kucoin->checkBalance();
+    $totalBalance = 0;
+
+  
+    foreach($getBalances['data'] as $index) { //go through each coin you have
+          $available = $index['available']; 
+		  $currency = $index['currency'];
+        //   echo $available.' '.$index['currency'].' ';
+
+          if($available > 0) { //check for available balance
+				
+				if($index['currency'] == 'USDT') {
+					$USDTBalance = $available; 
+					$totalBalance += $USDTBalance; //add to totalBalance
+					$bid = 1;
+				}
+				else {
+					$pair = $currency.'-USDT'; //XRP-USDT
+					$getPrices = $Kucoin->getMarketPrice($pair); //get bid price
+					$bid = $getPrices['data']['bestBid'];
+					
+					$USDTBalance = $available * $bid;
+	  
+					$totalBalance += $USDTBalance; //add to totalBalance
+				}
+
+				$output .= '<tr><td>'.$currency.'</td><td>'.$available.'</td><td>'.$bid.'</td><td>'.$btcValue.'</td><td>'.$USDTBalance.'</td></tr>';
+          }
+    }
+  
+    //orders only take 4 decimals
+    $buyQT = number_format($buyQT, 4, '.', '');
+    $sellQT = number_format($sellQT, 4, '.', '');
+    $coinBalance = number_format($coinBalance, 4, '.', '');
+    $USDTBalance = number_format($USDTBalance, 4, '.', '');
+    $totalBalance = number_format($totalBalance, 4, '.', '');
+  
+    echo $USDTBalance.' '.$totalBalance;
+  
+
+	return $output;
 }
 
 if ($_GET['page'] == 'kucoinMainBalance') {
-	
-	$key = $kucoin1_key;
-	$secret = $kucoin1_secret;
-	$passphrase = $kucoin1_passphrase;   
-	
-	$getBalances = checkBalance();
 
-    displayKCBalance($getBalances);
-exit;
-	$totalBalance = 0;
+	$apiFields = array(
+		'apiKey' => $kucoin1_key,
+		'apiSecret' => $kucoin1_secret,
+		'passphrase' => $kucoin1_passphrase
+	);
 
-	foreach($getBalances['data'] as $index) { //go through each coin you have
-    $available = $index['available'];
-   
-	if($index['currency'] == $coin[1] && $available > 0) { //match coin symbol   
-			$coinBalance = $available; 
-			if($index['available'] > 0) { //check for available balance
-				$sellQT = $index['available']; 
-				$totalBalance += $sellQT * $bid;
-			}
-		}
+	echo displayKCTable('kucoinMainBalance', $apiFields);
 
-		if($index['currency'] == 'USDT' && $available > 0) {
-			$USDTBalance = $available; 
-			$totalBalance += $USDTBalance; //add to totalBalance
-			$buyQT = $USDTBalance/$ask; //quantity to buy
-			$buyQT = $buyQT * $percentBalance; 
-		}
-	}
-
-	//orders only take 4 decimals
-	$buyQT = number_format($buyQT, 4, '.', '');
-	$sellQT = number_format($sellQT, 4, '.', '');
-	$coinBalance = number_format($coinBalance, 4, '.', '');
-	$USDTBalance = number_format($USDTBalance, 4, '.', '');
-	$totalBalance = number_format($totalBalance, 4, '.', '');
-
-	echo $USDTBalance.' '.$totalBalance;
 }
 else if ($_GET['page'] == 'kucoin1Balance') {
-	$key = $kucoin1_key;
-	$secret = $kucoin1_secret;
-	$passphrase = $kucoin1_passphrase;   
-	
-	$getBalances = checkBalance();
+
+	$apiFields = array(
+		'apiKey' => $kucoin1_key,
+		'apiSecret' => $kucoin1_secret,
+		'passphrase' => $kucoin1_passphrase
+	);
+
+	echo displayKCTable('kucoin1Balance', $apiFields);
 
 }
 else if ($_GET['page'] == 'kucoin2Balance') {
 
-	$key = $kucoin2_key;
-	$secret = $kucoin2_secret;
-	$passphrase = $kucoin2_passphrase;   
-	
-	$getBalances = checkBalance();
+	$apiFields = array(
+		'apiKey' => $kucoin2_key,
+		'apiSecret' => $kucoin2_secret,
+		'passphrase' => $kucoin2_passphrase
+	);
+
+	echo displayKCTable('kucoin2Balance', $apiFields);
+
 	
 }
-else if ($_GET['page'] == 'kucoin2Balance') {
+else if ($_GET['page'] == 'kucoin3Balance') {
 
-    $key = $kucoin3_key;
-	$secret = $kucoin3_secret;
-	$passphrase = $kucoin3_passphrase;   
-	
-	$getBalances = checkBalance();
+	$apiFields = array(
+		'apiKey' => $kucoin3_key,
+		'apiSecret' => $kucoin3_secret,
+		'passphrase' => $kucoin3_passphrase
+	);
+
+	echo displayKCTable('kucoin3Balance', $apiFields);
+
 }
-else if ($_GET['page'] == 'kucoin2Balance') {
+else if ($_GET['page'] == 'kucoin4Balance') {
+	$apiFields = array(
+		'apiKey' => $kucoin4_key,
+		'apiSecret' => $kucoin4_secret,
+		'passphrase' => $kucoin4_passphrase
+	);
 
-    $key = $kucoin4_key;
-	$secret = $kucoin4_secret;
-	$passphrase = $kucoin4_passphrase;   
-	
-	$getBalances = checkBalance();
+	echo displayKCTable('kucoin4Balance', $apiFields);
 }
-else if ($_GET['page'] == 'kucoin2Balance') {
+else if ($_GET['page'] == 'kucoin5Balance') {
+	$apiFields = array(
+		'apiKey' => $kucoin5_key,
+		'apiSecret' => $kucoin5_secret,
+		'passphrase' => $kucoin5_passphrase
+	);
 
-    $key = $kucoin5_key;
-	$secret = $kucoin5_secret;
-	$passphrase = $kucoin5_passphrase;   
-	
-	$getBalances = checkBalance();
+	echo displayKCTable('kucoin5Balance', $apiFields);
 }
 
 ?>
