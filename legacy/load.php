57a34a1c420d689f539ab2973e8ccc6522c1c8fa
TheJadeEmperor <benjamin_louie@drugfree.org>
2021-04-01 19:01:48 -0400

    move load.php to legacy folder

--- legacy/load.php
+++ legacy/load.php
@@ -4,6 +4,7 @@ include($dir.'functions.php');
 include($dir.'config.php');
 include($dir.'api_database.php');
 include($dir.'api_bittrex.php');
+include($dir.'api_kucoin.php');
 
 //set timezone
 date_default_timezone_set('America/New_York');
@@ -13,60 +14,6 @@ if($_GET['accessKey'] != 'AllLivesMatter') {
 }
 	
 $bittrex = new Client ($bittrex_api_key, $bittrex_api_secret);
-
-
-// $tableData = new Database($conn);
-
-// $condTable = $tableData->alertsTable();
-
-// $tradesTable = $tableData->tradesTable();
-
-// $optionsTable = $tableData->getSettingsFromDB();
-
-
-
-foreach($optionsTable as $option) {
-	if($option->opt == 'notes') 
-		$notes = $option->setting;
-}
-
-
-if($_GET['page'] == 'notes') {
-
-	$loadNotesAjax = 'include/ajax.php?action=updateNotes';
-	$loadNotes = 'load.php?page=notes&accessKey='.$accessKey;
-	?>
-	<script>
-	function updateNotes() {
-		$.ajax({ //Process the form using $.ajax()
-			type        : 'POST', //Method type
-			url         : '<?=$loadNotesAjax?>', //Your form processing file url
-			data        : $('#notesForm').serialize(), 
-			success     : function(msg) {
-				console.log( msg );
-				alert(msg);
-				reloadNotes();
-			}
-		});
-		event.preventDefault(); //Prevent the default submit      
-	}
-	
-	$(document).ready(function () {
-		$( "input[name=updateNotes]" ).click(function() {
-			updateNotes();
-		});
-	});
-	</script>
-
-	<div id="notesArea">
-		<form id="notesForm">
-		<textarea rows="7" cols="50" name="notes"><?=$notes?></textarea><br />
-		<input type="button" class="btn btn-success" name="updateNotes" value="Submit">
-		</form>
-	</div>
-	<?
-}
-
 
 
 $conditionDropDown = '<select name="on_condition"><option value=">"> > </option><option value="<"> < </option></select>';
@@ -117,109 +64,8 @@ $actionTypes = array(
 foreach($actionTypes as $aType) {
 	$actionDropDown .= '<option value="'.$aType.'">'.$aType.'</option>'; 
 }
+
 $tradeActionDropDown = '<select name="trade_action">'.$actionDropDown.'</option>';
-
-
-
-	
-function showPoloBalanceTable($polo, $tableTitle) {
-	?>
-	
-	
-	<table class="table">
-		<thead class="thead-default">
-			<tr>
-				<th colspan="8"><?=$tableTitle?> <img src="include/refresh.png" class="clickable" onclick="javascript:reloadTable('#balanceTable')" width="25px" /> </th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<th>Currency</th><th>Balance</th><th>Price</th><th>Change</th><th>BTC Value</th><th>USDT</th>
-			</tr>
-	<?php
-	
-	$balanceArray = $polo->get_balances();
-
-	$tickerArray = $polo->get_ticker();
-	
-//	echo '<pre>'; print_r($balanceArray); echo '</pre>';
-	
-	foreach($balanceArray as $currency => $currencyBalance) {
-		if($currencyBalance > 0.01) {
-		
-			$btcPrice = $tickerArray['USDT_BTC']['last'];
-			
-			if($currency == 'BTC') {
-				$chartLink = 'BTCUSD';
-				$currencyPair = 'USDT_BTC';
-				$lastFormat = $tickerArray[$currencyPair]['last'];
-				
-				$lastFormat = '$'.number_format($lastFormat, 2);
-				$btcValue = $currencyBalance;		
-				$usdtValue = $btcValue * $btcPrice;	
-				
-			}
-			else if($currency == 'USDT') {
-				$chartLink = 'BTCUSD';
-				$currencyPair = 'USDT_BTC';
-				$lastFormat = $tickerArray[$currencyPair]['last'];;
-				$btcValue = $currencyBalance / $lastFormat;	
-				$usdtValue = $currencyBalance;
-				$lastFormat = 1;
-			}
-			else { 
-				$chartLink = $currency.'BTC';
-				$currencyPair = 'BTC_'.$currency;
-				$lastFormat = $tickerArray[$currencyPair]['last'];
-				$lastFormat = number_format($lastFormat, 8);
-				$btcValue = $lastFormat * $currencyBalance;
-				$usdtValue = $btcValue * $btcPrice;
-			}
-			
-			$percentChange = $tickerArray[$currencyPair]['percentChange'];
-			$percentChangeFormat = $percentChange * 100;
-			$percentChangeFormat = number_format($percentChangeFormat, 2);
-			
-			$btcValueFormat = number_format($btcValue, 4);
-			$totalBTC += $btcValue;
-			$usdtValueFormat = number_format($usdtValue, 2);
-			
-			
-			if($percentChangeFormat > 0) $color = 'green';
-			else $color = 'red';
-			
-			if($currency == 'BTC' || $currency == 'ETH')
-				$formatting = 'style="font-weight: bold;"';
-			else
-				$formatting = 'style="font-weight: normal;"';
-	
-			if ($btcValueFormat > 0.01) {			
-				$balanceTable .= '<tr '.$formatting.'><td><a href="https://www.tradingview.com/chart/'.$chartLink.'" target="_BLANK">'.$currency.'</a></td>
-				<td>'.$currencyBalance.'</td>
-				<td>'.$lastFormat.'</td>
-				<td style="color: '.$color.'">'.$percentChangeFormat.'%</td>
-				<td>'.$btcValueFormat.'</td>
-				<td style="color: white">'.$usdtValueFormat.'</td>
-				</tr>';
-			}
-		}
-	}
-	echo $balanceTable;
-	
-	$totalBTCFormat = number_format($totalBTC, 8);
-	
-	$totalUSDT = $totalBTC * $btcPrice;
-	$totalUSDTFormat = number_format($totalUSDT, 2);
-	
-	echo '<tr><td colspan="10">Total BTC: '.$totalBTCFormat.' &nbsp;&nbsp; 
-	<span style="color: white">Total USDT: '.$totalUSDTFormat.'</span></td>';
-	
-	echo '</tbody>
-	</table>';
-
-}
-
-
 
 
 	/*
@@ -301,7 +147,7 @@ function showPoloBalanceTable($polo, $tableTitle) {
 	$coinbase_xrp_usd = number_format($coinbase_xrp_usd, 2);
 	
 	
-if($_GET['page'] == 'priceTable'){
+if($_GET['page'] == 'priceTable') {
 		
 	$bittrexURL = 'http://bestpayingsites.com/admin/btcTradingAPI/bittrex/';
 
@@ -519,13 +365,6 @@ else if($_GET['page'] == 'cronAutoTrade'){
 </div>
 	
 <?
-	
-}
-else if($_GET['page'] == 'balanceTable') {
-	
-	$tableTitle = 'Polo Balance';
-	
-	showPoloBalanceTable($polo, $tableTitle);
 
 }
 else if($_GET['page'] == 'btrexBalance') {
@@ -536,9 +375,7 @@ else if($_GET['page'] == 'btrexBalance') {
 	catch(Exception $e){
 		echo 'Error occurred : '.$e->getMessage(); 
 	}	
-	echo 'bittrex balance';
 	
-
 		?>
 		<table class="table">
 			<thead class="thead-default">
@@ -551,30 +388,30 @@ else if($_GET['page'] == 'btrexBalance') {
 		<?
 		
 		if(!empty($balance)) { 
-			foreach($balance as $x => $val) {
+			foreach($balance as $key => $val) {
 			
-				$currency = $val->Currency;
-				$currencyBalance = $val->Balance;
+				$currency = $val->Currency; //coin 
+				$currencyBalance = $val->Balance; //coin balance
 				
 				if($currency == 'BTXCRD') continue; //invalid market
 				if($currency == 'BTC' ) {
-					// echo $currency.' '.$currencyBalance;
 					$currencyPair = 'USDT-BTC';
-					$lastFormat = $bittrex_btc_usd_raw;
-					$btcValue = $currencyBalance;
+					$lastFormat = $bittrex_btc_usd_raw; //formatted price
+					$btcValue = $currencyBalance; //btc value
+					$usdtValue = $btcValue * $bittrex_btc_usd_raw; //get usdt value
 				}
 				else if ($currency == 'USDT') {
-					$lastFormat = 1;
-					$usdtValue = $currencyBalance;
+					$lastFormat = 1; //usdt has no price
+					$usdtValue = $currencyBalance; 
 					$btcValue = $currencyBalance / $bittrex_btc_usd_raw;
 				}
-				else {
-					$currencyPair = 'BTC-'.$currency;
-				
+				else { //all other coins
+					$currencyPair = 'BTC-'.$currency; //ticker pair
 					$ticker = $bittrex->getTicker($currencyPair);
+
 					$lastFormat = $ticker->Last;
-					$btcValue = $currencyBalance * $lastFormat;	
-					$usdtValue = $btcValue * $bittrex_btc_usd_raw;
+					$btcValue = $currencyBalance * $lastFormat;	//btc value
+					$usdtValue = $btcValue * $bittrex_btc_usd_raw; //get usdt value
 				}
 			
 				$btcValueFormat = number_format($btcValue, 4);
@@ -582,7 +419,7 @@ else if($_GET['page'] == 'btrexBalance') {
 				$totalBTC += $btcValue;
 				$totalUSD += $usdtValue;
 				
-				echo $currency.' '.$currencyBalance.' '.$usdtValue.' '.$ticker->Last.' '.$bittrex_btc_usd_raw.' <br />';
+				//echo $currency.' '.$currencyBalance.' '.$usdtValue.' '.$ticker->Last.' '.$bittrex_btc_usd_raw.' <br />';
 				
 				if($btcValue > 0.01 || $usdtValue > 1) {
 					echo '<tr><td>'.$currency.'</td>
@@ -592,18 +429,17 @@ else if($_GET['page'] == 'btrexBalance') {
 					<td><span style="color: gray">'.$usdtValueFormat.'</span></td>
 					</tr>';
 	
-				} //if($btcValue > 0.01 || $usdtValue > 1) 
+				} //if($btcValue > 0.01 || $usdtValue > 1)
 			} //foreach($balance as $x => $val)
 			 
 			$totalBTCFormat	= number_format($totalBTC, 4);
 			$totalUSDTFormat = number_format($totalUSD, 2);
 			 
 			echo '<tr><td colspan="10">Total BTC: '.$totalBTCFormat.' &nbsp;&nbsp; 
-			<span style="color: white">Total USDT: $'.$totalUSDTFormat.'</span></td>';
+			<span style="color: gray">Total USDT: $'.$totalUSDTFormat.'</span></td>';
 		
 			echo '</table>';
-		} //if(!empty($balance)) 
-		
+		} //if(!empty($balance)) 	
 }
 else if($_GET['page'] == 'bitmexPositions') {
 	
@@ -662,75 +498,7 @@ else if($_GET['page'] == 'bitmexPositions') {
 	
 <?
 }
-else if($_GET['page'] == 'bitmexPositions2') {
-	
-	$bitmex = new bitmex($bitmex2_api_key, $bitmex2_api_secret);
-	
-	$bitmexPos = $bitmex->getOpenPositions();
-	$getWallet = $bitmex->getWallet();
-	$totalSatoshiBalance = $totalSatoshiAvailable = $getWallet['amount'];
 
-/*
-	echo '<pre>';
-	print_r($bitmexPos[0]);
-	echo '</pre>';
-	
-	echo '<pre>';
-	print_r($getWallet);
-	echo '</pre>';
-	*/
-	
-	?>
-	<table class="table">
-		<thead class="thead-default">
-			<tr>
-				<th colspan="8">Bitmex-2 Positions <img src="include/refresh.png" class="clickable" onclick="javascript:reloadBitmexPositions2()" width="25px" /> </th>
-			</tr>
-			<tr>
-				<th>Symbol</th><th>openingQty</th><th>markPrice</th><th>avgEntryPrice</th><th>breakEvenPrice</th><th>unrealisedGrossPnl</th><th>liquidationPrice</th>
-			</tr>
-			
-		</thead>
-		<tbody>
-		
-		<?
-		//markPrice unrealisedGrossPnl
-		foreach($bitmexPos as $num => $pos) {
-			
-			$unrealizedPNL = $pos['unrealisedGrossPnl'];
-			
-			$totalSatoshiAvailable += $unrealizedPNL;
-			
-			$unrealizedPNL = $unrealizedPNL/100000000;
-			
-			$unrealizedPNLDisplay = $tableData->format_change_display($unrealizedPNL, 4);
-			
-			$openingQty = $tableData->format_change_display($pos['openingQty'], 2);
-			
-			
-			echo '<tr><td>'.$pos['symbol'].'</td>
-			<td>'.$openingQty.'</td>
-			<td>'.$pos['markPrice'].'</td>
-			<td>'.$pos['avgEntryPrice'].'</td>
-			<td>'.$pos['breakEvenPrice'].'</td>
-			<td>'.$unrealizedPNLDisplay.'</td>
-			<td>'.$pos['liquidationPrice'].'</td>
-			</tr>';
-		}
-		
-	$totalXBTBalance = $totalSatoshiBalance/100000000; 
-	$totalXBTAvailable = $totalSatoshiAvailable/100000000;
-	
-	echo '<tr><td colspan="3">BTC '.$totalXBTBalance.'  
-	Available BTC '.$totalXBTAvailable.'</td></tr>';
-	
-	?>
-		</tbody>
-	</table>
-	
-	<?
-	
-}
 
 
 
