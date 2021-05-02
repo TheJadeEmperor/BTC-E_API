79fb647e37ad1d402f1222ad7f187526b4311b2b
TheJadeEmperor <benjamin_louie@drugfree.org>
2021-05-02 11:12:17 -0400

    linksModule - add link for webhook data

--- load/linksModule.php
+++ load/linksModule.php
@@ -4,37 +4,35 @@ $loadingImage = '<img src="include/images/load.gif" id="loadingImage" />';
 
 <h2>Shortcuts <img src="include/images/refresh.png" class="clickable" onclick="javascript:reloadLinksModule()" width="25px" /></h2>
                     
-<br /><br />
+<br />
 <div class="row">
     <div class="col-sm-5">
 
-        <p align="left"><a href="https://www.tradingview.com/chart/gghdcAkO/" target="_blank">TradingView Chart 1</a>
+        <p><a href="https://www.tradingview.com/chart/gghdcAkO/" target="_blank">TradingView Chart 1</a></p>
 
-        <a href="https://www.tradingview.com/chart/HRbO8WTw/" target="_blank">TradingView Chart 2</a>
+        <p><a href="https://www.tradingview.com/chart/HRbO8WTw/" target="_blank">TradingView Chart 2</a></p>
 
-        <br /><br />
+        <p><a href="https://trade.kucoin.com/KCS-USDT" target="_blank">Kucoin Trade</a></p>
 
-        <a href="https://trade.kucoin.com/KCS-USDT" target="_blank">Kucoin Trade</a>
+        <p><a href="https://www.kucoin.com/account/sub" target="_blank">KC Subaccounts</a></p>
 
-        <br />
-
-        <a href="https://www.kucoin.com/account/sub" target="_blank">KC Subaccounts</a>
-        </p>
     </div>
     <div class="col-sm-5">
-        <p align="left">
-        <a href="http://btcapi.test/dashboard.php" target="_blank">BTC Dashboard Local</a><br />
-        <a href="http://code.bestpayingsites.com" target="_blank">BTC Dashboard Live</a>
-        
-        <br /><br />
 
-        <a href="http://btcapi.test/load/webhook.php" target="_blank">Webhook Data</a>
-        </p>
-    
+        <p><a href="http://btcapi.test/dashboard.php" target="_blank">BTC Dashboard Local</a></p>
+
+        <p><a href="http://code.bestpayingsites.com" target="_blank">BTC Dashboard Live</a></p>
+         
     </div>
 </div>
+<br />
 
-<p>Curl & Panic Curl</p>
+<h2>Curl & Panic Curl</h2>
 <a href="./curl.php" target="_BLANK"><button class="btn btn-success clickable">CURL Functions</button></a>
 
-<a href="curl_panic.php" target="_BLANK"><button class="btn btn-danger clickable">Panic Button</button></a>
+<a href="./curl_panic.php" target="_BLANK"><button class="btn btn-danger clickable">Panic Button</button></a>
+
+<p>&nbsp;</p>
+
+<h2>Webhook Data</h2>
+<a href="./load/webhook.php" target="_BLANK"><button class="btn btn-warning clickable">Webhook Data</button></a>
